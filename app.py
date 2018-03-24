76649b8d8d3cda6f2ed208301156b858e5dafd6c
Jason Zilberkweit <jlz293@nyu.edu>
2018-03-23 22:29:38 -0400

    change

--- app.py
+++ app.py
@@ -1,23 +1,34 @@
 from flask import Flask, request, render_template, send_from_directory
 import json
-import pymysql.cursors
+# import pymysql.cursors
 
 app = Flask(__name__)
 
-
-def get_database_connection():
-    return pymysql.connect(
-        host='localhost',
-        user='root',
-        password='password',
-        db='todo_database',
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor
-    )
-
-
+#
+# def get_database_connection():
+#     return pymysql.connect(
+#         host='localhost',
+#         user='root',
+#         password='password',
+#         db='todo_database',
+#         charset='utf8mb4',
+#         cursorclass=pymysql.cursors.DictCursor
+#     )
 
 
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/Shelter/")
+def shelter():
+    return render_template("Shelter.html")
+
+@app.route("/ShelterProvider/")
+def shelter():
+    return render_template("ShelterProvider.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
